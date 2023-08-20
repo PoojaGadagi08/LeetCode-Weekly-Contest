710a32129c72c872800694a690e306a12007f075
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-08-20 09:28:18 +0530

    Create 20-8-23 Contest No 359.java

---
+++ 20-8-23 Contest No 359.java
@@ -0,0 +1,141 @@
+1.Check if a String Is an Acronym of Words
+
+  Given an array of strings words and a string s, determine if s is an acronym of words.
+
+The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+
+Return true if s is an acronym of words, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: words = ["alice","bob","charlie"], s = "abc"
+Output: true
+Explanation: The first character in the words "alice", "bob", and "charlie" are 'a', 'b', and 'c', respectively. Hence, s = "abc" is the acronym. 
+Example 2:
+
+Input: words = ["an","apple"], s = "a"
+Output: false
+Explanation: The first character in the words "an" and "apple" are 'a' and 'a', respectively. 
+The acronym formed by concatenating these characters is "aa". 
+Hence, s = "a" is not the acronym.
+Example 3:
+
+Input: words = ["never","gonna","give","up","on","you"], s = "ngguoy"
+Output: true
+Explanation: By concatenating the first character of the words in the array, we get the string "ngguoy". 
+Hence, s = "ngguoy" is the acronym.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 10
+1 <= s.length <= 100
+words[i] and s consist of lowercase English letters.
+
+
+
+
+  class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        String str ="";
+        
+       for(int i=0;i<words.size();i++){
+           str+=words.get(i).charAt(0);
+       }
+        
+        if(str.equals(s))
+            return true;
+        
+        return false;
+    }
+}
+
+
+
+2.Determine the Minimum Sum of a k-avoiding Array
+
+  You are given two integers, n and k.
+
+An array of distinct positive integers is called a k-avoiding array if there does not exist any pair of distinct elements that sum to k.
+
+Return the minimum possible sum of a k-avoiding array of length n.
+
+ 
+
+Example 1:
+
+Input: n = 5, k = 4
+Output: 18
+Explanation: Consider the k-avoiding array [1,2,4,5,6], which has a sum of 18.
+It can be proven that there is no k-avoiding array with a sum less than 18.
+Example 2:
+
+Input: n = 2, k = 6
+Output: 3
+Explanation: We can construct the array [1,2], which has a sum of 3.
+It can be proven that there is no k-avoiding array with a sum less than 3.
+ 
+
+Constraints:
+
+1 <= n, k <= 50
+
+
+  class Solution {
+    public int minimumSum(int n, int k) {
+       Set<Integer> set=new HashSet<>();
+        int sum=0;
+        int i=1;
+        
+        while(set.size()<n){
+            if(!set.contains(k-i)){
+                set.add(i);
+                sum+=i;
+            }
+            i++;
+        }
+        return sum;
+    }
+}
+
+
+3.Maximize the Profit as the Salesman
+
+  You are given an integer n representing the number of houses on a number line, numbered from 0 to n - 1.
+
+Additionally, you are given a 2D integer array offers where offers[i] = [starti, endi, goldi], indicating that ith buyer wants to buy all the houses from starti to endi for goldi amount of gold.
+
+As a salesman, your goal is to maximize your earnings by strategically selecting and selling houses to buyers.
+
+Return the maximum amount of gold you can earn.
+
+Note that different buyers can't buy the same house, and some houses may remain unsold.
+
+ 
+
+Example 1:
+
+Input: n = 5, offers = [[0,0,1],[0,2,2],[1,3,2]]
+Output: 3
+Explanation: There are 5 houses numbered from 0 to 4 and there are 3 purchase offers.
+We sell houses in the range [0,0] to 1st buyer for 1 gold and houses in the range [1,3] to 3rd buyer for 2 golds.
+It can be proven that 3 is the maximum amount of gold we can achieve.
+Example 2:
+
+Input: n = 5, offers = [[0,0,1],[0,2,10],[1,3,2]]
+Output: 10
+Explanation: There are 5 houses numbered from 0 to 4 and there are 3 purchase offers.
+We sell houses in the range [0,2] to 2nd buyer for 10 golds.
+It can be proven that 10 is the maximum amount of gold we can achieve.
+ 
+
+Constraints:
+
+1 <= n <= 105
+1 <= offers.length <= 105
+offers[i].length == 3
+0 <= starti <= endi <= n - 1
+1 <= goldi <= 103
